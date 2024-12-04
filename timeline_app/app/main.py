605bd4d65c80cbc2e6852d3f45dc347e8fc7b8b5
t-walker-21 <twalker81@gatech.edu>
2024-12-04 10:20:20 -0500

    Add note for tweet sort

--- timeline_app/app/main.py
+++ timeline_app/app/main.py
@@ -37,6 +37,7 @@ def generate_timeline(session: Session = Depends(get_session), current_user: str
     for tweet in TweetDocument.objects(user_id__in=people_followed_str):
         tweets.append(json.loads(tweet.to_json()))
 
+    #TODO: Move this to the DB call
     tweets.sort(key=lambda x: x['created_at']['$date'], reverse=True)
         
     return {'tweets': tweets}
